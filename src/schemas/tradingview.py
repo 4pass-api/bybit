131816e41b536605a759d0b14382996771caa0c1
leo <luisleo52655@gmail.com>
2024-09-28 21:13:00 +0800

    Implement: Update

--- src/schemas/tradingview.py
+++ src/schemas/tradingview.py
@@ -1,4 +1,36 @@
+from enum import Enum, EnumMeta
+
 from pydantic import BaseModel, Field
 
+
+class MetaEnum(EnumMeta):
+    def __contains__(cls, item):
+        try:
+            cls(item)
+        except ValueError:
+            return False
+        return True
+
+
+class CustomStringEnum(str, Enum, metaclass=MetaEnum):
+    def __str__(self):
+        return self.value
+
+
+class SideEnum(CustomStringEnum):
+    BUY = "buy"
+    SELL = "sell"
+
+
+class ActionEnum(CustomStringEnum):
+    OPEN_POSITION_1 = "open_position_1"
+    CLOSE_POSITION_1 = "close_position_1"
+    OPEN_POSITION_2 = "open_position_2"
+    CLOSE_POSITION_2 = "close_position_2"
+
+
 class TradingViewRequest(BaseModel):
-    pass
+    side: SideEnum = Field(..., title="Side of the trade")
+    action: ActionEnum = Field(..., title="Action to take")
+    size: float = Field(..., title="Size of the trade")
+    symbol: str = Field(..., title="Symbol to trade")
--- src/server.py
+++ src/server.py
@@ -1,10 +1,64 @@
-from fastapi import FastAPI
+import os
+from contextlib import asynccontextmanager
+from typing import Annotated
+
+import ccxt
+from dotenv import load_dotenv
+from fastapi import FastAPI, Query, HTTPException, status, Security, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
+from fastapi.security import APIKeyHeader
 from starlette.requests import Request
 
-from src.routers.auth import router as auth_router
 from src.schemas.basic import TextOnly
+from src.schemas.tradingview import TradingViewRequest
+
+load_dotenv()
+
+exs = [ccxt.bybit({
+    'apiKey': os.getenv('BYBIT_APIKEY_1'),
+    'secret': os.getenv('BYBIT_SECRET_1')
+}), ccxt.bybit({
+    'apiKey': os.getenv('BYBIT_APIKEY_2'),
+    'secret': os.getenv('BYBIT_SECRET_2')
+})]
+
+api_key_header = APIKeyHeader(name='x-api-key', auto_error=False)
+
+
+def get_api_key(api_key: str = Security(api_key_header)) -> str:
+    if api_key != os.getenv('API_KEY', 'zxcvbnm1234'):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Could not validate credentials"
+        )
+    return api_key
+
+
+def get_ip(request: Request):
+    forwarded_for = request.headers.get("x-forwarded-for")
+    try:
+        client_ip = forwarded_for.split(",")[0]
+    except:
+        client_ip = request.client.host
+    return client_ip
+
+
+def request_from_tradingview(ip: str = Depends(get_ip)):
+    if ip not in ["52.89.214.238", "34.212.75.30", "54.218.53.128", "52.32.178.7"]:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail=f"Invalid IP: {ip}"
+        )
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Load the ML model
+    for ex in exs:
+        ex.load_markets()
+    yield
+
 
 app = FastAPI(
     title="Template FastAPI Backend Server",
@@ -13,7 +67,8 @@ app = FastAPI(
     contact={
         "name": "Author Name",
         "email": "example@exmaple.com",
-    }
+    },
+    lifespan=lifespan
 )
 
 app.add_middleware(
@@ -24,12 +79,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(auth_router, prefix="/auth", tags=["auth"])
-
 
 @app.get("/", response_model=TextOnly)
 async def root():
     return TextOnly(text="Hello World")
+
+
+@app.get('/ip')
+async def get_client_ip(ip: str = Depends(get_ip)):
+    return {"ip": ip}
 
 
 @app.get("/elements", include_in_schema=False)
@@ -54,3 +112,81 @@ async def api_documentation(request: Request):
 
   </body>
 </html>""")
+
+
+@app.get('/balance', dependencies=[Depends(get_api_key)])
+async def balance():
+    try:
+        return {idx: ex.fetch_balance() for idx, ex in enumerate(exs, 1)}
+    except Exception as e:
+        return HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e)
+        )
+
+
+@app.get('/positions', dependencies=[Depends(get_api_key)])
+async def positions(
+        symbol: Annotated[
+            str, Query(..., title="Symbol to get positions for", description="Symbol to get positions for")]
+):
+    try:
+        return {idx: ex.fetch_position(symbol) for idx, ex in enumerate(exs, 1)}
+    except Exception as e:
+        return HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e)
+        )
+
+
+@app.post('/leverage', dependencies=[Depends(get_api_key)])
+async def set_leverage(
+        symbol: Annotated[
+            str, Query(..., title="Symbol to set leverage for", description="Symbol to set leverage for")],
+        leverage: Annotated[
+            int, Query(..., title="Leverage to set", description="Leverage to set", ge=1, le=100)]
+):
+    try:
+        return {idx: ex.set_leverage(leverage, symbol) for idx, ex in enumerate(exs, 1)}
+    except Exception as e:
+        return HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e)
+        )
+
+
+@app.post('/setup', dependencies=[Depends(get_api_key)])
+async def setup_account(
+        symbol: Annotated[
+            str, Query(..., title="Symbol to setup account for", description="Symbol to setup account for")
+        ]
+):
+    try:
+        return {idx: ex.set_position_mode(False, symbol) for idx, ex in enumerate(exs, 1)}
+    except Exception as e:
+        return HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e)
+        )
+
+
+@app.get('/symbols')
+async def get_symbols_by_keyword(
+        keyword: Annotated[
+            str, Query(..., title="Keyword to search for", description="Keyword to search for")
+        ]
+):
+    all_symbols = list(exs[0].markets.keys())
+    filter_symbols = [symbol for symbol in all_symbols if keyword.lower() in symbol.lower()]
+    return [symbol for symbol in filter_symbols if
+            exs[0].markets[symbol]['active'] and exs[0].markets[symbol]['contract']]
+
+
+@app.post('/oneway', dependencies=[Depends(request_from_tradingview)])
+async def oneway_action(
+        payload: TradingViewRequest
+):
+    if '1' in payload.action:
+        exs[0].create_order(payload.symbol, 'market', str(payload.side), payload.size)
+    else:
+        exs[1].create_order(payload.symbol, 'market', str(payload.side), payload.size)
